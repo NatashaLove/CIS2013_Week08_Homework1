f83afb04ec05f6f7f826649bc09eed5a28e641e9
Natasha S Love <debora@inbox.ru>
2018-03-14 00:16:13 -0500

    Game Battleship
    1-player game. 3 different boards/fields

--- CIS2013_Week08_Homework1.cpp
+++ CIS2013_Week08_Homework1.cpp
@@ -1,53 +1,42 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
-//void new_game (int g);
 void print (char field [10][10]);
-//void u_board();
-//void s_count();
-//void coord(int& x, int& y);
-//void processing (char buffer2[][10], char field[][10]);
-
 
 int main () {
 	
 	int x, y;
 	char g;
-	//char cont = 'y';
 	
 	// Inputs/ prints ready numbers from the file
 	ifstream game;
-	ofstream moves;
-	//int l;
-	
-	char field [10][10];
-	char buffer1[10][10];
-	char buffer2 [10][10];
-	
-	// amount of 'x' 
+		
+	char display_field [10][10];
+	char game_buffer[10][10];
+		
+	// amount of 'x' in a game
 	int count = 20;
-	
-	//new_game(l);
-	
+	 
+	 //choose game	
 	cout << endl;
 	cout << "Hello! Choose your game: ENTER: 1, 2 or 3 " << endl;
 	cin >> g;
 	
-	switch (g)
-	{
-		case 1: game.open ("game1.dat");
-		break;
-		
-		case 2: game.open ("game2.dat");
-		break;
-		
-		case 3: game.open ("game3.dat");
-		break;
-	}
-	
-	//game.open ("game1.dat");
+	if (g == '1') {
+	game.open("game1.dat"); 
+	}else if (g == '2'){ 
+	game.open("game2.dat"); 
+	}else if (g == '3'){ 
+	game.open("game3.dat"); 
+	}else
+	{ 
+	cout << "Could not find the file"; 
+	exit(1); 
+	} 
+
 	
 	if(game.fail()) {
 		cout << "Something went wrong with " << g << endl;
@@ -56,48 +45,35 @@ int main () {
 	cout << "Let's START!";
 	cout << endl;
 }
-	
-	//int count;
-	
-	game >> buffer1 [10][10];
-	// char f;
-	// char b2;
-	
-	print(field);
-	
-	//u_board();
-	
-	for(int i=0;i<10;i++) {
-	
-	for(int j=0; j<10; j++) {
-		buffer2[i][j]=' ';
+
+// Create temporary array, where game is loaded (one line)
+	char temp_buffer[100];
+	game >> temp_buffer;
+
+	// Transform temp array into 2D array (board)
+	for (int x = 0; x < 10; x++)
+	{
+		for (int y = 0; y < 10; y++)
+		{
+			game_buffer[y][x] = temp_buffer[y * 10 + x];
 		}
-		
 	}
 	
-	//s_count();
+	// Print empty field
 	
-	//char cell;
-	//char* cell = (char*)field;
+	for(int x=0; x<10; x++) {
+	for(int y=0; y<10; y++) {
+		display_field[x][y] = '.';
+	}
 	
-	// Ship/hit count.
-	//char* cell = (char*)field[];
+}
 	
-	//buffer2[a][b]=field[a][b];
-	
+	print(display_field);
 		
-	// for (int i = 0; i < 100;i++)
-	// {
-		// if (buffer2[i] == 'x')
-			// count++;
-	// }
-
 	
+	// ask user for coordinates
 	while(1) {
-		moves.open("board.dat");
-		moves << buffer2 [10][10] << endl;
 		
-		//coord(x,y);
 		cout << endl;
 		cout << "Please enter the coordinates between 0 and 9 " << endl;
 		cout << endl;
@@ -105,11 +81,18 @@ int main () {
 		cin >> x;
 		cout << "y =";
 		cin >> y;
+		cout << endl;
+		
+		 if (cin.fail()) {
+		cout << "Wrong value!";
+		
+		exit (1);
+		 }
 		
 		if (x > 9 || x < 0 || y < 0 || y > 9)
 		{
-			cout << "Wrong value. ";
-			//coord(x,y);
+		cout << "Wrong value. ";
+			
 		cout << endl;
 		cout << "Please enter the coordinates between 0 and 9 " << endl;
 		cout << endl;
@@ -118,108 +101,50 @@ int main () {
 		cout << "y =";
 		cin >> y;
 	}
-		
-	 //   processing (buffer2, field); 
-	
-	if (buffer2[x][y] == ' ')
+				// Check if this coordinate has been already used
+		if (display_field[x][y] != '.')
 		{
-			if (field[x][y] == 'x')
-			{
-				cout << "HIT! " << endl;
-				buffer2[x][y] = 'X';
-				count--;
-				if (count==0)
-				{
-					print(buffer2);
-					cout << endl;
-					cout << "The enemy is defeated. You won.";
-					//exit (1);
-					break;
-				}
-			}
-			else
-			{
-				cout << "MISS! ";
-				buffer2[x][y] = '0';
-			}
-			cout << endl;
-			print(buffer2);
-			cout << "--------------------------------------------";
+			cout << "Already tried these coordinates. Enter other coordinates. " << endl;
 		}
 		else
 		{
-			cout << "Already hit there! Try other coordinates.";
-		}
+			// Check for hit or miss and update and print display_field	
+	
+			if (game_buffer[x][y] == 'x')
+			{
+				display_field[x][y] = 'X';
+				cout << "HIT! " << endl;
+				count--;
+				print(display_field);\
+				cout << endl;				
+				if (count==0)
+				{
+					print(display_field);
+					cout << endl;
+					cout << "The enemy is defeated. You won.";
+					
+					break;
+				}
+			}
+			else {
+			
+				cout << "MISS! ";
+				display_field[x][y] = '0';
+		
+			cout << endl;
+			print(display_field);
+			
+			} 
 	}
 	
-	//cout << buffer1;
-	
-	// cout << "Choose your game: ENTER: 1, 2 or 3 " << endl;
-	// cin >> g;
-	
-	// switch (g)
-	// {
-		// case 1: game.open ("game1.dat");
-		// break;
-		
-		// case 2: game.open ("game2.dat");
-		// break;
-		
-		// case 3: game.open ("game3.dat");
-		// break;
-	// }
-	
-	// //game.open ("game1.dat");
-	// if(game.fail()) {
-		// cout << "Something went wrong with " << g << endl;
-		// exit(1);
-// } else {
-	// cout << "Let's START!";
-// }
-
-//char* tmp = (char*)board;
-
+cout << "--------------------------------------------";
+	}
 return 0; 
 }
 
-// void new_game (int g) {
-	// ifstream game;
-	// //int g;
-	// cout << endl;
-	// cout << "Hello! Choose your game: ENTER: 1, 2 or 3 " << endl;
-	// cin >> g;
-	
-	// switch (g)
-	// {
-		// case 1: game.open ("game1.dat");
-		// break;
-		
-		// case 2: game.open ("game2.dat");
-		// break;
-		
-		// case 3: game.open ("game3.dat");
-		// break;
-	// }
-	
-	// //game.open ("game1.dat");
-	// if(game.fail()) {
-		// cout << "Something went wrong with " << g << endl;
-		// exit(1);
-// } else {
-	// cout << "Let's START!";
-	// cout << endl;
-// }
-	
-// }
-void print (char field [10][10]) {
-	//char field[10][10];
 
-for(int x=0; x<10; x++) {
-	for(int y=0; y<10; y++) {
-		field[x][y] = '.';
-	}
+void print (char field [10][10]) {
 	
-}
 cout << endl << "		  0 1 2 3 4 5 6 7 8 9" << endl;
 for(int x=0; x<10; x++) {
 	cout << "		" <<x << " ";
@@ -231,79 +156,3 @@ for(int x=0; x<10; x++) {
 }
 }
 
-// Create empty array for recording the player's moves
-// void u_board(){
-// char field [10][10]; 
-// char buffer2 [10][10] ;
-// //char buffer2 [10][10];
-
-// // buffer2[0][0]=' ';
-// for(int i=0;i<10;i++) {
-	
-	// for(int j=0; j<10; j++) {
-		// buffer2[i][j]=' ';
-	// }
-		
-// }
-// }
-
-// void s_count() {
-	// char field;
-	// char cell;
-	// char* cell = (char*)field;
-	
-	// int count = 0;
-	// for (int i = 0; i < 100;i++)
-	// {
-		// if (cell[i] == 'x')
-			// count++;
-	// }
-// }
-
-// void coord(int& x, int& y) {
-	
-	// cout << endl;
-	// cout << "Please enter the coordinates between 0 and 9 " << endl;
-	// cout << endl;
-	// cout << "x = ";
-	// cin >> x;
-	// cout << "y =";
-	// cin >> y;
-// }
-
-// void processing (char buffer2[][10], char field[][10], int &x, int &y)  {
-	// //char buffer2;
-	// //char field;
-	// int count;
-	// //int &x; 
-	// //int &y;
-	
-	// if (buffer2[x][y] == ' ')
-		// {
-			// if (field[x][y] == 'x')
-			// {
-				// cout << "HIT! " << endl;
-				// buffer2[x][y] = 'X';
-				// count--;
-				// if (!count)
-				// {
-					// print(buffer2);
-					// cout << endl;
-					// cout << "The enemy is defeated. You won.";
-					// exit (1);
-				// }
-			// }
-			// else
-			// {
-				// cout << "MISS! ";
-				// buffer2[x][y] = '0';
-			// }
-			// cout << endl;
-			// print(buffer2);
-			// cout << "----------------------------------------";
-		// }
-		// else
-		// {
-			// cout << "Already hit there! Try other coordinates.";
-		// }
-// }
